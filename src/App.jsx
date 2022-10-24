3721ad98d90a49602f35886dec4cdcfe52d2c7c1
guisilva10 <107316736+guisilva10@users.noreply.github.com>
2022-10-24 05:47:01 -0300

    feat: initial quiz pro

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useContext,useEffect } from 'react'
+import { QuizContext } from './context/quiz'
+
+
+import Questions from './components/Questions'
+import Welcome from './components/Welcome'
+import GameOver from './components/GameOver'
+
+
+import './App.css'
+import PickCategory from './components/PickCategory'
+
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext)
+
+  useEffect(() => {
+    dispatch({type: "REORDER_QUESTIONS"})
+  }, [])
+
+  return (
+    <div className="App">
+     <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome/>}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Questions/>}
+      {quizState.gameStage === "End" && <GameOver/>}
+    </div>
+  )
+}
+
+export default App
